f44efd8e39b71f1d5bd08f07a7fb05791ebd4d48
kky <kykim9050@naver.com>
2024-05-31 18:21:27 +0900

    김경윤 : TestLevelDefaultpawn 추가 및 Buile.cs 파일에 경로추가

--- Source/PracticePJ/PracticePJ.Build.cs
+++ Source/PracticePJ/PracticePJ.Build.cs
@@ -7,7 +7,9 @@ public class PracticePJ : ModuleRules
 	public PracticePJ(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
+		PublicIncludePaths.Add(System.IO.Path.Combine(ModuleDirectory, ""));
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
---
+++ Source/PracticePJ/TestLevel/TestLevelDefaultPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DefaultPawn.h"
+#include "TestLevelDefaultPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICEPJ_API ATestLevelDefaultPawn : public ADefaultPawn
+{
+	GENERATED_BODY()
+	
+};
