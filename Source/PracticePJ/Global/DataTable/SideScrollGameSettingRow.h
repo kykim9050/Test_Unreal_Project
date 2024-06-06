9995fda4ea6c27217dcc842c09a2732727f5b14c
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-06 22:07:03 +0900

    김경윤 : SideScrollSettingRow 클래스 추가 및 에디터 창에 데이터테이블 세팅

---
+++ Source/PracticePJ/Global/DataTable/SideScrollGameSettingRow.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "SideScrollGameSettingRow.generated.h"
+
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FSideScrollPlayerData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parameter")
+	float PlayerSpeed = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameter")
+	float PlayerJump = 1000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameter")
+	float PlayerGravityRatio = 1.0f;
+};
+
+
+USTRUCT(BlueprintType)
+struct FSideScrollGameSettingRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameter")
+	FSideScrollPlayerData PlayerData = FSideScrollPlayerData();
+};
