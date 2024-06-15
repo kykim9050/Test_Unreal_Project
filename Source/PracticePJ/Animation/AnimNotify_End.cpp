31538bf700ef996b1f08ade6f50a866c881ff99c
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-15 17:16:11 +0900

    노티파이 클래스로 만들어서 애니메이션 변경

---
+++ Source/PracticePJ/Animation/AnimNotify_End.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Animation/AnimNotify_End.h"
+#include "Global/Animation/GlobalAnimInstance.h"
+
+UAnimNotify_End::UAnimNotify_End()
+{
+#if WITH_EDITOR
+	bShouldFireInEditor = false;
+#endif
+}
+
+void UAnimNotify_End::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	UGlobalAnimInstance* AnimInst = Cast<UGlobalAnimInstance>(MeshComp->GetAnimInstance());
+
+	if (nullptr != AnimInst)
+	{
+		AnimInst->ChangeAnimation(NextAnimationName);
+	}
+}
