210a6931556aede981cdd1a1915a63bbc20c96d2
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-20 15:14:54 +0900

    중간 커밋

--- Source/PracticePJ/FPS/Object/FPS_WeaponComponent.cpp
+++ Source/PracticePJ/FPS/Object/FPS_WeaponComponent.cpp
@@ -2,26 +2,119 @@
 
 
 #include "FPS/Object/FPS_WeaponComponent.h"
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "FPSTest/FPSCharacter.h"
+//#include "FPSTestProjectile.h"
+#include "GameFramework/PlayerController.h"
+#include "Camera/PlayerCameraManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Animation/AnimInstance.h"
+#include "Engine/LocalPlayer.h"
+#include "Engine/World.h"
 
-// Sets default values
-AFPS_WeaponComponent::AFPS_WeaponComponent()
+// Sets default values for this component's properties
+UFPS_WeaponComponent::UFPS_WeaponComponent()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	// Default offset from the character location for projectiles to spawn
+	MuzzleOffset = FVector(100.0f, 0.0f, 10.0f);
 }
 
-// Called when the game starts or when spawned
-void AFPS_WeaponComponent::BeginPlay()
+
+//void UFPS_WeaponComponent::Fire()
+//{
+//	if (Character == nullptr || Character->GetController() == nullptr)
+//	{
+//		return;
+//	}
+//
+//	// Try and fire a projectile
+//	if (ProjectileClass != nullptr)
+//	{
+//		UWorld* const World = GetWorld();
+//		if (World != nullptr)
+//		{
+//			APlayerController* PlayerController = Cast<APlayerController>(Character->GetController());
+//			const FRotator SpawnRotation = PlayerController->PlayerCameraManager->GetCameraRotation();
+//			// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+//			const FVector SpawnLocation = GetOwner()->GetActorLocation() + SpawnRotation.RotateVector(MuzzleOffset);
+//
+//			//Set Spawn Collision Handling Override
+//			FActorSpawnParameters ActorSpawnParams;
+//			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+//
+//			// Spawn the projectile at the muzzle
+//			World->SpawnActor<AFPSTestProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+//		}
+//	}
+//
+//	// Try and play the sound if specified
+//	if (FireSound != nullptr)
+//	{
+//		UGameplayStatics::PlaySoundAtLocation(this, FireSound, Character->GetActorLocation());
+//	}
+//
+//	// Try and play a firing animation if specified
+//	if (FireAnimation != nullptr)
+//	{
+//		// Get the animation object for the arms mesh
+//		UAnimInstance* AnimInstance = Character->GetMesh1P()->GetAnimInstance();
+//		if (AnimInstance != nullptr)
+//		{
+//			AnimInstance->Montage_Play(FireAnimation, 1.f);
+//		}
+//	}
+//}
+
+bool UFPS_WeaponComponent::AttachWeapon(AFPSCharacter* TargetCharacter)
 {
-	Super::BeginPlay();
-	
+	Character = TargetCharacter;
+
+	// Check that the character is valid, and has no weapon component yet
+	if (Character == nullptr || Character->GetInstanceComponents().FindItemByClass<UFPS_WeaponComponent>())
+	{
+		return false;
+	}
+
+	// Attach the weapon to the First Person Character
+	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, true);
+	//AttachToComponent(Character->GetMesh1P(), AttachmentRules, FName(TEXT("GripPoint")));
+
+	// add the weapon as an instance component to the character
+	Character->AddInstanceComponent(this);
+
+	// Set up action bindings
+	if (APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Set the priority of the mapping to 1, so that it overrides the Jump action with the Fire action when using touch input
+			Subsystem->AddMappingContext(FireMappingContext, 1);
+		}
+
+		//if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		//{
+		//	// Fire
+		//	EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &UFPS_WeaponComponent::Fire);
+		//}
+	}
+
+	return true;
 }
 
-// Called every frame
-void AFPS_WeaponComponent::Tick(float DeltaTime)
+void UFPS_WeaponComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	Super::Tick(DeltaTime);
+	if (Character == nullptr)
+	{
+		return;
+	}
 
+	if (APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->RemoveMappingContext(FireMappingContext);
+		}
+	}
 }
-
--- Source/PracticePJ/FPS/Object/FPS_WeaponComponent.h
+++ Source/PracticePJ/FPS/Object/FPS_WeaponComponent.h
@@ -3,24 +3,60 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "FPS_WeaponComponent.generated.h"
 
-UCLASS()
-class PRACTICEPJ_API AFPS_WeaponComponent : public AActor
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PRACTICEPJ_API UFPS_WeaponComponent : public USkeletalMeshComponent
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	AFPS_WeaponComponent();
+public:
+	///** Projectile class to spawn */
+	//UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	//TSubclassOf<class AFPSTestProjectile> ProjectileClass;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimMontage* FireAnimation;
+
+	/** Gun muzzle's offset from the characters location */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	FVector MuzzleOffset;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* FireMappingContext;
+
+	/** Fire Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* FireAction;
+
+	/** Sets default values for this component's properties */
+	UFPS_WeaponComponent();
+
+	/** Attaches the actor to a FirstPersonCharacter */
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	bool AttachWeapon(class AFPSCharacter* TargetCharacter);
+
+	///** Make the weapon Fire a Projectile */
+	//UFUNCTION(BlueprintCallable, Category = "Weapon")
+	//void Fire();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	/** Ends gameplay for this component. */
+	UFUNCTION()
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	/** The Character holding this weapon*/
+	AFPSCharacter* Character;
 
 };
