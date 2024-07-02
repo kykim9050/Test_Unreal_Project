62999480c443c1164b7c8425406829cca4419e00
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-02 18:48:58 +0900

    중간 커밋

---
+++ Source/PracticePJEditor/KKYEditorUtilityWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KKYEditorUtilityWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICEPJEDITOR_API UKKYEditorUtilityWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
