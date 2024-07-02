4a55dbc0acc2727cab8a13ad7c080c49cc8accfb
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-02 18:35:28 +0900

    모듈 파일 추가 및 기본 설정

---
+++ Source/PracticePJEditor/PracticePJEditor.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PracticePJEditor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PracticePJEditor, "PracticePJEditor" );
